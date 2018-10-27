c8ad6732c1f8e92bcde8aa490bc0f532205fb4fc
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-10-27 12:07:02 +0400

    Feat: basic useState and useEffect test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background: palegoldenrod;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const StyledInput = styled.input`
+  font-size: 1.5rem;
+  background: rgba(50, 50, 50, 0.5);
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 20px;
+  margin: 10px;
+  color: #eee;
+`
 
 function App() {
+  const [name, setName] = useState('Guga')
+  const [surname, setSurname] = useState('Gongadze')
+
+  useEffect(() => {
+    document.title = `${name} ${surname}`
+  })
+
   return (
-    <div>
-      <h3>helloo</h3>
-    </div>
+    <Container>
+      <StyledInput value={name} onChange={(e) => setName(e.target.value)} />
+      <StyledInput value={surname} onChange={(e) => setSurname(e.target.value)} />
+    </Container>
   )
 }
 
